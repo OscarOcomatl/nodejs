f4e235a5c984be6b28c7630999fb066b45e05682
Oscar <oscarperezocomatldev@gmail.com>
2025-03-26 23:16:08 -0600

    patron-adaptador: Seccion patron adaptador terminada

--- 02-bases/src/js-foundation/08-patron-adaptador-fetchAPI.js
+++ 02-bases/src/js-foundation/08-patron-adaptador-fetchAPI.js
@@ -3,13 +3,20 @@ const {http} = require('../plugins')
 
 
 // Promesa con una mejor manera de ser implementada
-const getPokemonById = async ( id ) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    
+const getPokemonById = async ( param ) => {
+    var url = `https://pokeapi.co/api/v2/pokemon/`;
+
+    if ( typeof param === 'string' )
+        url = url+`${param}`;
+        console.log('string ',url)
+    if ( typeof param === 'number' )
+        url = url+`${param}`;
+        console.log(url)
     const pokemon = await http.get(url);
-
-
-    return pokemon.name;
+    return {
+        name:pokemon.name,
+        abilities:pokemon.abilities
+    };
 }
 
 module.exports = getPokemonById
